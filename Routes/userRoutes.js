052a9664767485d2b2a60dd6fa0dcf611628f81f
rcbala <balasubramani18663@gmail.com>
2024-03-13 14:33:26 +0530

    add bcryptjs

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { generateToken, getByEmail } from "../Controllers/userController.js";
 import User from "../Models/userModel.js";
 
